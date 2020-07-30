600f6d274dcb48d5826fb7031a163f8bd9baf8b6
shibicr93 <shibicr93@gmail.com>
2020-07-30 20:27:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
-import os
-
 import tensorflow as tf
 from flask import Flask, render_template, request, send_from_directory,redirect
-import keras_preprocessing
-from keras_preprocessing import image
-from keras_preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import Conv2D,MaxPooling2D
-from tensorflow.keras.layers import Activation, Dense, Flatten, Dropout
-from tensorflow.keras.applications.vgg16 import VGG16
-import numpy as np
-import json
-import urllib.request
 
 app = Flask(__name__)
 classes = ["Bathroom Mirrors", "Bathroom Shelves", "Bathroom Sinks", "Ceiling Fans", "Curtain Rods", "Pet Beds", "Rugs", "Toilet Paper Holders", "Towel Bars", "Towel Hooks", "Towel Racks", "Towel Rings"]
-transfer_learning_model = tf.keras.models.load_model("snaplookupv2.h5")
-
+transfer_learning_model = None
 # home page
 @app.route("/")
 def home():
   return render_template("test.html")
-                  
+
 
 @app.route('/predict',methods=['POST'])
 def predict():
+    global transfer_learning_model
+    if not transfer_learning_model :
+        print("loading model..")
+        transfer_learning_model = tf.keras.models.load_model("snaplookupv2.h5")
+    else :
+        print("model is already loaded")
     file = request.files['imgfile']
     preprocessed_image=tf.image.decode_jpeg(file.read(),channels=3)
     preprocessed_image=tf.image.resize(preprocessed_image,[100,100])
@@ -36,4 +30,3 @@ def predict():
 
 if(__name__=='__main__'):
   app.run(port=8080,debug='true',threaded=False)
-
